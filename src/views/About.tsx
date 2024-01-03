0d3e2cde73e36560f06f935c6c9e962f9ad7f946
Catrovitch <ale.engelhardt@outlook.com>
2024-01-03 16:10:50 +0200

    About ready

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -17,7 +17,55 @@ import { fadeIn, scale } from "../utils/variants";
 import { transition } from "../utils/transition";
 
 const About = () => {
-  return <div>About</div>;
-};
+  return (
+  <div 
+    className="min-h-screen flex items-center justify-center relative"
+    style={{
+      background: `url(${aboutPageImg})`,
+      backgroundPosition: "center",
+      backgroundRepeat: "no repeat",
+      backgroundSize: "cover",
+    }}
+  >
+    <div 
+      className="max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between
+      items-center xl:items-start gap-12 w-full py-16 px-12"
+    >
+      <div className="flex-1 flex flex-col gap-4">
+        <h2 className="text-center xl:text-start text-4xl sm:text-5xl lg:text-[64px] 
+            font-bold text-textPrimary">
+          About <span className="text-secondary"> me</span>
+        </h2>
+
+        <p className="text-center xl:text-start text-base sm:text-lg text-textSecondary">
+          Hi, my name is Alexander, and I have a wide set of interests from philosophy and history
+          to arts to technology. On this website I will gather some of my thoughts on various topics.
+        </p>
+
+        <div className="flex items-center justify-center xl:justify-start gap-6">
+          <SocialMediaIcon imgSrc={facebookIcon} title="Facebook"></SocialMediaIcon>
+          <SocialMediaIcon imgSrc={instagramIcon} title="Instagram"></SocialMediaIcon>
+          <SocialMediaIcon imgSrc={twitterIcon} title="Twitter"></SocialMediaIcon>
+          <SocialMediaIcon imgSrc={youtubeIcon} title="YouTube"></SocialMediaIcon>
+        </div>
+
+  
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+          <img 
+            src={aboutIllustration}
+            alt=""
+            className="max-w-full sm:max-w-[401px]"
+          />
+        </div>
+        
+    </div>
+    
+    <div className="absolute botton-0 left-0 w-full h-[1px] bg-divider" />
+    
+    </div>
+    
+  )
+  };
 
 export default About;
