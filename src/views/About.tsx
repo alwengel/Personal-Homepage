8080d96f86a63b797040965bbd4c23289f74c857
Catrovitch <ale.engelhardt@outlook.com>
2024-01-08 15:10:52 +0200

    updated styling of Computer Science page

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,4 +1,7 @@
-// asse
+// assets
+import skillCircle from "../assets/skill-circle.svg"
+import heroIllustration from "../assets/hero-illustration.svg"
+
 // components
 import { Reveal } from "../components";
 
@@ -9,6 +12,7 @@ import { motion } from "framer-motion";
 import { fadeIn, scale } from "../utils/variants";
 import { transition } from "../utils/transition";
 import { generateBackgroundSVG } from "../utils/backgroundSVG";
+
 
 const About = () => {
 
@@ -38,35 +42,30 @@ const About = () => {
       <div className="max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between items-center xl:items-start gap-12 w-full py-16 px-12">
 
   
-        {/* Three Columns */}
         <div className="flex-1 flex flex-col xl:flex-row gap-4">
-          {/* Studies */}
+
+          
           <div className="flex-1">
             <Reveal>
               <h3 className="text-center xl:text-start text-2xl sm:text-3xl lg:text-4xl font-bold text-textWhite">
-                <span className="text-secondary">Studies</span>
+                <span className="text-secondary">Interests</span>
               </h3>
-            </Reveal>
+            
             <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
               Hi, my name is Alexander Engelhardt. I have a wide set of interests ranging from philosophy and history to arts and technology.
               I am studying the Master's Programme in Computer Science at the University of Helsinki with a focus on Software Architecture,
               Distributed Systems, and Full Stack Development. I also have a minor in geography entailing natural geography, urban planning, and geographical information systems.
             </p>
-          </div>
-  
-          {/* Arts & Design */}
-          <div className="flex-1">
-            <Reveal>
-              <h3 className="text-center xl:text-start text-2xl sm:text-3xl lg:text-4xl font-bold text-textWhite">
-                <span className="text-secondary">Arts & Design</span>
-              </h3>
-            </Reveal>
+
             <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
               Outside of my studies, I have a strong interest in several forms of art. I am currently finishing my first musical concept album which
               embodies my experiences in music and of the world up to this point, mixing pop, rock, jazz, classical, hip hop and ambience. I am also directing/screenwriting a
               semi-professional surrealistic horror comedy film which is set to release sometime this year.
             </p>
+            </Reveal>
           </div>
+  
+
   
           {/* Personality */}
           <div className="flex-1">
@@ -74,12 +73,33 @@ const About = () => {
               <h3 className="text-center xl:text-start text-2xl sm:text-3xl lg:text-4xl font-bold text-textWhite">
                 <span className="text-secondary">Personality</span>
               </h3>
+            
+              <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
+                As a person, I am driven by my will to create. I love nothing more than to focus on a product, making it the best it can be. This shows in my long history of always having some project going on.
+              </p>
+
+              <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
+                Besides creativity, I love working together with other people. In projects, I often find myself in leadership positions due to idea richness coupled with strong communication skills. I have found the key to good leadership to be balancing between being assertive while never forsaking the humbleness of listening to other people and reaching group decisions.
+
+                Another perspective on leadership that I like is that a good leader is also a good servant. What I take from this saying is that a good leader achieves the best outcome by enabling team members to reach their own best potential. This also goes along with the quote of Lao Tzu:
+
+                "A leader is best when people barely know he exists, when his work is done, his aim fulfilled, they will say: we did it ourselves."
+              </p>
             </Reveal>
-            <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
-              Hi, my name is Alexander Engelhardt. I have a wide set of interests ranging from philosophy and history to arts and technology.
-              I am studying the Master's Programme in Computer Science at the University of Helsinki with a focus on Software Architecture,
-              Distributed Systems, and Full Stack Development. I also have a minor in geography entailing natural geography, urban planning, and geographical information systems.
-            </p>
+          </div>
+
+          <div className="flex-1">
+            <Reveal>
+              <motion.img
+                variants={scale()}
+                transition={transition()}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: false}}
+                src={skillCircle} alt="" className="max-h-[548px]"
+              />
+            </Reveal>
+            
           </div>
         </div>
       </div>
--- src/views/ComputerScience.tsx
+++ src/views/ComputerScience.tsx
@@ -1,5 +1,6 @@
 // assets
-import aboutIllustration from "../assets/about-illustration.svg";
+import alexandersComputerSetUp from "../assets/Alexanders_Computer_Set_up_2024.jpg";
+
 
 // components
 import { Reveal } from "../components";
@@ -8,74 +9,85 @@ import { Reveal } from "../components";
 import { motion } from "framer-motion";
 
 // utils
-import { scale } from "../utils/variants";
+import { fadeIn, scale } from "../utils/variants";
 import { transition } from "../utils/transition";
 import { generateBackgroundSVG } from "../utils/backgroundSVG";
 
-const ComputerScience = () => {
+
+const About = () => {
 
   const svgString = generateBackgroundSVG("#00c0b2")
 
   return (
-  <div
-    id="computer-science"
-    className="min-h-screen flex items-center justify-center relative"
-    style={{
-      background: `url("data:image/svg+xml;utf8,${encodeURIComponent(svgString)}")`,
-      backgroundPosition: "center",
-      backgroundRepeat: "no repeat",
-      backgroundSize: "cover",
-    }}
-  >
-    <div 
-      className="max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between
-      items-center xl:items-start gap-12 w-full py-16 px-12"
+    <div
+      id="about"
+      className="min-h-screen flex items-center justify-center relative"
+      style={{
+        background: `url("data:image/svg+xml;utf8,${encodeURIComponent(svgString)}")`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
     >
-      <div className="flex-1 flex flex-col gap-4">
-        <Reveal>
-          <h2 className="text-center xl:text-start text-4xl sm:text-5xl lg:text-[64px] 
-              font-bold text-textWhite">
-            Computer <span className="text-secondary"> Science</span>
-          </h2>
-        </Reveal>
+      <div className="max-w-screen-2xl  xl:justify-between items-center xl:items-start gap-12 w-full py-16 px-12">
 
-        <Reveal>        
-        <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
-          Just some years ago I would not have seen myself loving computer science. Though I had always faired well in all subjects in school
-          I used to feel that my true calling was in arts, people skills and big picture thinking. In upper secondary school I seriously considered 
-          a career as a musician. When studying for the final exams and more specifically Geography, I noticed how I noticed how my increased
-          understanding of the world also led to greater meaning in my artistic works. This led to me applying and getting accepted to the bachelor's 
-          degree in geography.
+        <motion.div className="text-center xl:text-start">
+          <Reveal>
+            <h2 className="text-4xl sm:text-5xl lg:text-[64px] font-bold text-textWhite">
+              Computer <span className="text-secondary">Science</span>
+            </h2>
+          </Reveal>
+        </motion.div>
 
-        </p>
+      <div className="max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between items-center xl:items-start gap-12 w-full py-16 px-12">
 
-        <div className="mb-4" />
-
-        <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
-          Then how come I have ended up studying Computer science? The answer to this is that during my studies of geography I found my self appriciating the
-          technical courses the most as I felt that these gave me the most valuable and tangible skills. This culminated in me switching from the bachelor's 
-          programme in geography to the bachelor's programme in computer science. Ever since I have felt that computer science has given growth to my 
-          character where I not only posess a keen artistic eye, big picture analytical thinking that geography entails, but also robust technical insight.
-        </p>
-
-        </Reveal>
   
-      </div>
-      <div className="flex-1 flex items-center justify-center">
-          <motion.img
-            variants={scale()}
-            transition={transition()}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: false}}
-            src={aboutIllustration}
-            alt=""
-            className="max-w-full sm:max-w-[401px]"
-          />
-        </div>  
+        <div className="flex-1 flex flex-col xl:flex-row gap-4">
+
+          
+          <div className="flex-1">
+            <Reveal>        
+            <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
+              Just some years ago I would not have seen myself loving computer science. Though I had always faired well in all subjects in school
+              I used to feel that my true calling was in arts, people skills and big picture thinking. In upper secondary school I seriously considered 
+              a career as a musician. When studying for the final exams and more specifically Geography, I noticed how I noticed how my increased
+              understanding of the world also led to greater meaning in my artistic works. This led to me applying and getting accepted to the bachelor's 
+              degree in geography.
+
+            </p>
+          
+
+            <div className="mb-4" />
+
+            <p className="text-left xl:text-start text-base sm:text-lg text-textSecondary">
+              Then how come I have ended up studying Computer science? The answer to this is that during my studies of geography I found my self appriciating the
+              technical courses the most as I felt that these gave me the most valuable and tangible skills. This culminated in me switching from the bachelor's 
+              programme in geography to the bachelor's programme in computer science. Ever since I have felt that computer science has given growth to my 
+              character where I not only posess a keen artistic eye, big picture analytical thinking that geography entails, but also robust technical insight.
+            </p>
+
+            </Reveal>
+          </div>
+          <div className="flex-1">
+            <div className="flex-1 flex items-center justify-center">
+              <motion.img
+                variants={scale()}
+                transition={transition()}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: false}}
+                src={alexandersComputerSetUp}
+                alt=""
+                className="max-w-full sm:max-w-[401px]"
+                style={{ border: '2px solid #333' }}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-   )
-  };
+    </div>
+  );
+}
 
-export default ComputerScience;
+export default About;
